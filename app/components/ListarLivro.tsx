c3618da0fdf0008314e6d96058e487c1205763a8
christal <christal.b.camillo@gmail.com>
2025-09-21 15:26:37 -0300

    feat: componente para renderizar as estrelas de avaliação

--- app/components/ListarLivro.tsx
+++ app/components/ListarLivro.tsx
@@ -1,8 +1,32 @@
-import { Book } from '@/types/books';
+import Link from "next/link";
+import { Book } from "@/types/books";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 interface ListarLivroProps {
   books: Book[];
 }
+
+const StarRating = ({ rating }: { rating: number }) => {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <span
+        key={i}
+        className={i <= rating ? "text-yellow-400" : "text-gray-400"}
+      >
+        ★
+      </span>
+    );
+  }
+  return <div className="flex">{stars}</div>;
+};
 
 export default function ListarLivro({ books }: ListarLivroProps) {
   return (
@@ -11,29 +35,27 @@ export default function ListarLivro({ books }: ListarLivroProps) {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {books.map((book) => (
-          <div key={book.id} className="bg-gray-800 rounded-lg shadow-lg overflow-hidden flex flex-col items-center p-4">
-            <img 
-              src={book.cover} 
-              alt={`Capa do livro ${book.title}`} 
+          <div
+            key={book.id}
+            className="bg-gray-800 rounded-lg shadow-lg overflow-hidden flex flex-col items-center p-4"
+          >
+            <img
+              src={book.cover}
+              alt={`Capa do livro ${book.title}`}
               className="w-full h-auto object-cover rounded-md mb-4"
             />
             <div className="text-center">
-              <h2 className="text-xl font-semibold text-white mb-1">{book.title}</h2>
+              <h2 className="text-xl font-semibold text-white mb-1">
+                {book.title}
+              </h2>
               <p className="text-sm text-gray-400 mb-2">Autor: {book.author}</p>
-              
-              <p className="text-sm text-gray-400">
-                Gênero: {book.genre}
-              </p>
-              <p className="text-sm text-gray-400">
-                Ano: {book.year}
-              </p>
+
+              <p className="text-sm text-gray-400">Gênero: {book.genre}</p>
+              <p className="text-sm text-gray-400">Ano: {book.year}</p>
               <p className="text-sm text-gray-400">
                 Avaliação: {book.rating} estrelas
               </p>
-              <p className="text-sm text-gray-400">
-                Status: {book.status}
-              </p>
-              
+              <p className="text-sm text-gray-400">Status: {book.status}</p>
             </div>
           </div>
         ))}
